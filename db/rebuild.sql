9460587975e678f10ce1e8978d617c4575fd7c2e
CWP <77127867+Camwp@users.noreply.github.com>
2025-09-07 17:58:34 -0700

    rebuild

--- db/rebuild.sql
+++ db/rebuild.sql
@@ -317,3 +317,20 @@ WHERE inv_make = 'GM'
 UPDATE public.inventory
 SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
     inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
+INSERT INTO public.account (
+        account_firstname,
+        account_lastname,
+        account_email,
+        account_password
+    )
+VALUES (
+        'Tony',
+        'Stark',
+        'tony@starkent.com',
+        'Iam1ronM@n'
+    );
+UPDATE public.account
+SET account_type = 'Admin'
+WHERE account_email = 'tony@starkent.com';
+DELETE FROM public.account
+WHERE account_email = 'tony@starkent.com';
